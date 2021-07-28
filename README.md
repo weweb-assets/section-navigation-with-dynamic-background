74203138673cbf26e68277369455c00d8505fd42
fbriou <florian@weweb.io>
2021-07-28 19:18:55 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# FIRSTBASE-NAVBAR
+
+This is an section for [weweb.io](https://www.weweb.io/).
+
+## Installation
+
+To run locally, first install all dependencies with `npm install`.
+
+## Start
+
+To serve locally, run `npm run serve --port=[PORT]`, and then go to Weweb editor, open developper popup and add localhost:[PORT] as custom section.
+
+## Build
+
+Before release, you can check build error by running `npm run build --name="firstbase-navbar" --type="section"`
---
+++ src/wwSection.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="navigation-menu" :style="cssVariables">
+        <div
+            class="navigation-default"
+            :style="{ display: !isMenuDisplayed ? 'block' : 'none' }"
+            ww-responsive="desktop"
+        >
+            <wwLayout class="navigation-default__layout" path="navbarContent">
+                <template #default="{ item }">
+                    <wwLayoutItem>
+                        <wwElement v-bind="item" :states="states"></wwElement>
+                    </wwLayoutItem>
+                </template>
+            </wwLayout>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        content: { type: Object, required: true },
+        wwFrontState: { type: Object, required: true },
+    },
+    wwDefaultContent: {
+        navbarContent: [],
+    },
+    data() {
+        return {
+            states: [],
+            backgroundColor: this.content.backgroundColor,
+            resizeObserver: null,
+            resizeListenerHolder: null,
+            scrollListenerHolder: null,
+        };
+    },
+    computed: {
+        cssVariables() {
+            return {
+                '--backgroundColor': this.backgroundColor,
+            };
+        },
+    },
+    mounted() {
+        this.init();
+
+        this.resizeObserver = new ResizeObserver(() => {
+            this.getNavbarData();
+        });
+
+        this.resizeObserver.observe(this.$el);
+    },
+    beforeUnmount() {
+        this.resizeObserver.unobserve(this.$el);
+        this.removeListeners();
+    },
+    methods: {
+        init() {
+            this.refreshObservers();
+
+            if (!this.scrollListenerHolder)
+                this.scrollListenerHolder = document.addEventListener('scroll', this.refreshObservers);
+            if (!this.resizeListenerHolder)
+                this.resizeListenerHolder = document.addEventListener('resize', this.refreshObservers);
+        },
+        refreshObservers() {
+            this.scrollListener();
+        },
+        scrollListener() {
+            if (window.scrollY > 0) {
+                this.backgroundColor = this.content.backgroundColorWhenUserStartsScrolling;
+            } else {
+                this.backgroundColor = this.content.backgroundColor;
+            }
+        },
+        removeListeners() {
+            if (this.scrollListenerHolder)
+                this.scrollListenerHolder = document.removeEventListener('scroll', this.refreshObservers);
+            if (this.resizeListenerHolder)
+                this.resizeListenerHolder = document.removeEventListener('resize', this.refreshObservers);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.navigation-menu {
+    --backgroundColor: rgb(250, 250, 250);
+
+    position: fixed;
+    z-index: 9;
+    display: flex;
+    background-color: var(--backgroundColor);
+    transition: all 300ms;
+
+    .navigation-default {
+        position: relative;
+        width: 100%;
+        &__layout {
+            display: flex;
+            flex-direction: column;
+            min-width: 100px;
+        }
+    }
+}
+</style>
